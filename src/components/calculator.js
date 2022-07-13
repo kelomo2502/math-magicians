74e497a4ff942da814e2d30eaa8b9811fe5d51d4
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-07-13 19:48:20 +0100

    Add event to Calculator.js

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,49 +1,55 @@
-import React from 'react';
+// import React from 'react';
+import React, { useState } from 'react';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+import calculate from '../logic/calculate';
 
-  render() {
-    return (
-      <div className="App">
-        <div className="container">
-          <div className="display">
-            <span>0</span>
-          </div>
-          <div className=" row1">
-            <button type="button">AC </button>
-            <button type="button">+/-</button>
-            <button type="button">%</button>
-            <button className="operator" type="button">÷</button>
-          </div>
-          <div className="row2">
-            <button type="button">7</button>
-            <button type="button">8</button>
-            <button type="button">9</button>
-            <button className="operator" type="button">x</button>
-          </div>
-          <div className="row3">
-            <button type="button">4</button>
-            <button type="button">5</button>
-            <button type="button">6</button>
-            <button className="operator" type="button">-</button>
-          </div>
-          <div className="row4">
-            <button type="button">1</button>
-            <button type="button">2</button>
-            <button type="button">3</button>
-            <button className="operator" type="button">+</button>
-          </div>
-          <div className="row5">
-            <button type="button" className="zero">0</button>
-            <button type="button" className="operator">=</button>
-          </div>
+const Calculator = () => {
+  const display = document.getElementById('display');
+  const [result, setResult] = useState({ total: display, next: 0, operation: '' });
+
+  const clickFunction = (e) => {
+    setResult(calculate(result, e.target.innerHTML));
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="display" id="display">
+          {result.total}
+          {result.operation}
+          {result.next}
+        </div>
+        <div className=" row1">
+          <button onClick={clickFunction} type="button" value="AC">AC</button>
+          <button onClick={clickFunction} type="button" value="+">+/-</button>
+          <button onClick={clickFunction} type="button" value="%">%</button>
+          <button className="operator" onClick={clickFunction} type="button" value="÷">÷</button>
+        </div>
+        <div className="row2">
+          <button onClick={clickFunction} type="button" value="7">7</button>
+          <button onClick={clickFunction} type="button" value="8">8</button>
+          <button onClick={clickFunction} type="button" value="9">9</button>
+          <button onClick={clickFunction} className="operator" type="button" value="x">x</button>
+        </div>
+        <div className="row3">
+          <button onClick={clickFunction} type="button" value="4">4</button>
+          <button onClick={clickFunction} type="button" value="5">5</button>
+          <button onClick={clickFunction} type="button" value="6">6</button>
+          <button onClick={clickFunction} className="operator" type="button" value="-">-</button>
+        </div>
+        <div className="row4">
+          <button onClick={clickFunction} type="button" value="1">1</button>
+          <button onClick={clickFunction} type="button" value="2">2</button>
+          <button onClick={clickFunction} type="button" value="3">3</button>
+          <button onClick={clickFunction} className="operator" type="button" value="+">+</button>
+        </div>
+        <div className="row5">
+          <button onClick={clickFunction} type="button" className="zero" value="0">0</button>
+          <button onClick={clickFunction} type="button" className="operator" value="=">=</button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 export default Calculator;
