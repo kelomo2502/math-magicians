934c1c1c2aa029a9cba716da1f6372ed48d57404
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-07-19 10:54:51 +0100

    Add quote component

---
+++ src/components/Quote.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Quote() {
+  return (
+    <>
+      <blockquote cite="https://www.prodigygame.com/main-en/blog/math-quotes/">
+        <h2 style={{
+          width: '54rem', margin: '0 auto', color: '#00c2cb', fontFamily: 'Poppins',
+        }}
+        >
+          Mathematics is not about numbers, Equations, computations, or algorithims:
+          its about understanding
+        </h2>
+      </blockquote>
+      <h3 style={{ width: '54rem', margin: '0 auto', fontFamily: 'Poppins' }}>
+        <strong>William Paul Thompson</strong>
+      </h3>
+    </>
+  );
+}
